a91addf8b79a8bfd6bcc03d954a415c075a88aeb
Bick Renan <renanbickdev@gmail.com>
2024-07-12 18:56:56 -0300

    cors

--- api/app.ts
+++ api/app.ts
@@ -15,35 +15,6 @@ const prisma = new PrismaClient();
 // load .env environment variables
 dotenv.config();
 
-app.post('/api/login', async (req, res) => {
-    const { email, password } = req.body;
-    const user = await prisma.user.findFirst({
-        where: { email },
-    });
-    if (!user || !compareSync(password, user.password)) {
-        res.status(401).json({ error: 'Invalid credentials' });
-    } else {
-        // sign a JWT token and return it in the response
-        const token = jwt.sign({ sub: user.id }, process.env.JWT_SECRET!);
-        res.json({ id: user.id, email: user.email, token });
-    }
-});
-
-function getUser(req: Request) {
-    const token = req.headers.authorization?.split(' ')[1];
-    if (!token) {
-        return undefined;
-    }
-    try {
-        const decoded: any = jwt.verify(token, process.env.JWT_SECRET!);
-        return { id: decoded.sub };
-    } catch {
-        // bad token
-        return undefined;
-    }
-}
-
-
 app.listen(3000, () =>
     console.log('🚀 Server ready at: http://localhost:3000')
 );
@@ -52,7 +23,7 @@ app.use(
     '/api',
     ZenStackMiddleware({
         getPrisma: (req) => {
-            return enhance(prisma, { user: getUser(req) });
+            return enhance(prisma);
         },
     })
 );
--- api/index.ts
+++ api/index.ts
@@ -32,40 +32,11 @@ app.use(
     '/api',
     ZenStackMiddleware({
         getPrisma: (req) => {
-            return enhance(prisma, { user: getUser(req) });
+            return enhance(prisma);
         },
         handler: apiHandler
     })
 );
 
-
-app.post('/api/login', async (req, res) => {
-    const { email, password } = req.body;
-    const user = await prisma.user.findFirst({
-        where: { email },
-    });
-    if (!user || !compareSync(password, user.password)) {
-        res.status(401).json({ error: 'Invalid credentials' });
-    } else {
-        // sign a JWT token and return it in the response
-        const token = jwt.sign({ sub: user.id }, process.env.JWT_SECRET!);
-        res.json({ id: user.id, email: user.email, token });
-    }
-});
-
-function getUser(req: Request) {
-    const token = req.headers.authorization?.split(' ')[1];
-    console.log('TOKEN:', token);
-    if (!token) {
-        return undefined;
-    }
-    try {
-        const decoded: any = jwt.verify(token, process.env.JWT_SECRET!);
-        return { id: decoded.sub };
-    } catch {
-        // bad token
-        return undefined;
-    }
-}
 
 export default app;
