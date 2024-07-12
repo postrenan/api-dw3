507a7c864134b70f0a7f3d47023255b6f72b4486
Bick Renan <renanbickdev@gmail.com>
2024-07-12 10:50:27 -0300

    cors

--- api/app.ts
+++ api/app.ts
@@ -6,10 +6,12 @@ import dotenv from 'dotenv';
 import type { Request } from 'express';
 import express from 'express';
 import jwt from 'jsonwebtoken';
-
+// @ts-ignore
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors);
 
 const prisma = new PrismaClient();
 
