f72260b120713b01722917844348dc013bc0c1a2
Bick Renan <renanbickdev@gmail.com>
2024-07-14 18:44:21 -0300

    SEED

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,9 @@
 import { PrismaClient, Prisma } from '@prisma/client';
-import {randomUUID} from "node:crypto";
+import { randomUUID } from 'node:crypto';
 
 const prisma = new PrismaClient();
 
-const categoryData: Prisma.CategoriaCreateInput[]=[
+const categoryData: Prisma.CategoriaCreateInput[] = [
     {
         id: '1',
         nome: 'Higiene',
@@ -15,97 +15,131 @@ const categoryData: Prisma.CategoriaCreateInput[]=[
     {
         id: '3',
         nome: 'Suprimento',
-    }
+    },
 ];
 
-const userData: Prisma.UserCreateInput[]=[
+const userData: Prisma.UserCreateInput[] = [
     {
         id: '1',
         email: 'renan@gmail.com',
-        name:  'Renan Bick',
-        password : 'abc123'
+        name: 'Renan Bick',
+        password: 'abc123',
     },
     {
         id: '2',
         email: 'thales@gmail.com',
-        name:  'Thales Laggeman',
-        password : 'abc123'
-    }
-]
+        name: 'Thales Laggeman',
+        password: 'abc123',
+    },
+];
 
 const itemData: Prisma.ItemCreateInput[] = [
     {
         id: randomUUID(),
         name: 'Sabonete',
-        description: 'caixa de sabonetes contendo 10 unidade',
+        description: 'caixa de sabonetes contendo 10 unidades',
         qtd: 10,
         peso: '1Kg',
         dataVal: '2024-07-02T20:40:37Z',
         perecivel: false,
         user: {
-            connect:{
-                id: '1'
-            }
+            connect: {
+                id: '1',
+            },
         },
-        category : {
-            connect:{
-                id: '1'
-            }
-        }
+        itemCategoria: {
+            create: [
+                {
+                    categoria: {
+                        connect: { id: '1' },
+                    },
+                },
+            ],
+        },
     },
     {
         id: randomUUID(),
         name: 'Detergente',
-        description: 'Detergentes de 500 ml cada da marca bom brilho cheiro de coco',
+        description: 'Detergentes de 500 ml cada da marca Bom Bril, cheiro de coco',
         qtd: 25,
         peso: '12.5Kg',
         dataVal: '2024-07-02T20:40:37Z',
         perecivel: false,
         user: {
-            connect:{
-                id: '1'
-            }
+            connect: {
+                id: '1',
+            },
         },
-        category : {
-            connect:{
-                id: '2'
-            }
-        }
+        itemCategoria: {
+            create: [
+                {
+                    categoria: {
+                        connect: { id: '2' },
+                    },
+                },
+            ],
+        },
     },
     {
         id: randomUUID(),
         name: 'Arroz',
-        description: 'pacotes de arroz da marca bom arroz 4kg cada pacote',
+        description: 'pacotes de arroz da marca Bom Arroz, 4kg cada pacote',
         qtd: 10,
         peso: '40Kg',
         dataVal: '2024-07-02T20:40:37Z',
         perecivel: true,
         user: {
-            connect:{
-                id: '2'
-            }
+            connect: {
+                id: '2',
+            },
         },
-        category : {
-            connect:{
-                id: '3'
-            }
-        }
+        itemCategoria: {
+            create: [
+                {
+                    categoria: {
+                        connect: { id: '3' },
+                    },
+                },
+            ],
+        },
     },
 ];
 
 
+const estoqueData: Prisma.EstoqueCreateInput[] = [
+    {
+        id: randomUUID(),
+        item: {
+            connect: { id: itemData[0].id },
+        },
+        user: {
+            connect: { id: '1' },
+        },
+    },
+    {
+        id: randomUUID(),
+        item: {
+            connect: { id: itemData[1].id },
+        },
+        user: {
+            connect: { id: '1' },
+        },
+    },
+    {
+        id: randomUUID(),
+        item: {
+            connect: { id: itemData[2].id },
+        },
+        user: {
+            connect: { id: '2' },
+        },
+    },
+];
+
 async function main() {
     console.log(`Start seeding ...`);
 
-    for (const u of userData) {
-        const user = await prisma.user.upsert({
-            where: { id: u.id },
-            create: u,
-            update: {},
-        });
-        console.log(`Upserted User with id: ${user.id}`);
-    }
-
+    // Seed para categorias
     for (const c of categoryData) {
         const category = await prisma.categoria.upsert({
             where: { id: c.id },
@@ -115,6 +149,17 @@ async function main() {
         console.log(`Upserted category with id: ${category.id}`);
     }
 
+    // Seed para usuários
+    for (const u of userData) {
+        const user = await prisma.user.upsert({
+            where: { id: u.id },
+            create: u,
+            update: {},
+        });
+        console.log(`Upserted User with id: ${user.id}`);
+    }
+
+    // Seed para itens
     for (const i of itemData) {
         const item = await prisma.item.upsert({
             where: { id: i.id },
@@ -124,15 +169,23 @@ async function main() {
         console.log(`Upserted Item with id: ${item.id}`);
     }
 
+    // Seed para estoque
+    for (const e of estoqueData) {
+        const estoque = await prisma.estoque.upsert({
+            where: { id: e.id },
+            create: e,
+            update: {},
+        });
+        console.log(`Upserted Estoque with id: ${estoque.id}`);
+    }
+
     console.log(`Seeding finished.`);
 }
 
 main()
-    .then(async () => {
-        await prisma.$disconnect();
-    })
-    .catch(async (e) => {
+    .catch((e) => {
         console.error(e);
+    })
+    .finally(async () => {
         await prisma.$disconnect();
-        process.exit(1);
     });
