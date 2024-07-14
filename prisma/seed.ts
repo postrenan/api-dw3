888e003a2fd93fc0b303fe7739e3d17c811a1d0f
Bick Renan <renanbickdev@gmail.com>
2024-07-14 16:11:02 -0300

    cors

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,42 +3,93 @@ import {randomUUID} from "node:crypto";
 
 const prisma = new PrismaClient();
 
+const categoryData: Prisma.CategoriaCreateInput[]=[
+    {
+        id: '1',
+        nome: 'Higiene',
+    },
+    {
+        id: '2',
+        nome: 'Limpeza',
+    },
+    {
+        id: '3',
+        nome: 'Suprimento',
+    }
+];
+
+const userData: Prisma.UserCreateInput[]=[
+    {
+        id: '1',
+        email: 'renan@gmail.com',
+        name:  'Renan Bick',
+        password : 'abc123'
+    },
+    {
+        id: '2',
+        email: 'thales@gmail.com',
+        name:  'Thales Laggeman',
+        password : 'abc123'
+    }
+]
+
 const itemData: Prisma.ItemCreateInput[] = [
     {
         id: randomUUID(),
         name: 'Sabonete',
-        category: 'Higiene',
+        categoryId: '1',
         description: 'caixa de sabonetes contendo 10 unidade',
         qtd: 10,
-        kgl: 1,
+        peso: '1Kg',
         dataVal: '2024-07-02T20:40:37Z',
         perecivel: false,
+        userId: '1',
     },
     {
         id: randomUUID(),
         name: 'Detergente',
-        category: 'Limpeza',
+        categoryId: '2',
         description: 'Detergentes de 500 ml cada da marca bom brilho cheiro de coco',
         qtd: 25,
-        kgl: 12.5,
+        peso: '12.5Kg',
         dataVal: '2024-07-02T20:40:37Z',
-        perecivel: false
+        perecivel: false,
+        userId: '1',
     },
     {
         id: randomUUID(),
         name: 'Arroz',
-        category: 'Suprimento',
+        categoryId: '3',
         description: 'pacotes de arroz da marca bom arroz 4kg cada pacote',
         qtd: 10,
-        kgl: 40,
+        peso: '40Kg',
         dataVal: '2024-07-02T20:40:37Z',
-        perecivel: true
+        perecivel: true,
+        userId: '1',
     },
 ];
 
 
 async function main() {
     console.log(`Start seeding ...`);
+
+    for (const u of userData) {
+        const user = await prisma.user.upsert({
+            where: { id: u.id },
+            create: u,
+            update: {},
+        });
+        console.log(`Upserted User with id: ${user.id}`);
+    }
+
+    for (const c of categoryData) {
+        const category = await prisma.categoria.upsert({
+            where: { id: c.id },
+            create: c,
+            update: {},
+        });
+        console.log(`Upserted category with id: ${category.id}`);
+    }
 
     for (const i of itemData) {
         const item = await prisma.item.upsert({
